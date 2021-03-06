b286bf684ab67ddbf0094328b96d3329a50753de
Paul <quaifep@yahoo.com>
2021-03-05 23:09:10 -0800

    More Correct?

--- AddThreeGame.py
+++ AddThreeGame.py
@@ -2,55 +2,52 @@
 # Date: 3/1/2021
 # Description: Plays a game that allows two players to play a game in which they alternately choose numbers from 1-9.
 
-class AddThreeGame:
+class AddThreeGame(object):
+    pass
+    game = AddThreeGame()
+    def __init__(self):
+        """initial def that assigns players."""
+        self.__p1 = 0
+        self.__p2 = 0
+        self.__played = []
+        self.__str = "UNFINISHED"
 
-def __init__(self):
-    """initial def that assigns players."""
-    self.__p1 = 0
-    self.__p2 = 0
-    self.__played = []
-    self.__str = "UNFINISHED"
+    def get_current_state(self):
+        """returns current state of game"""
 
-def get_current_state(self):
-    """returns current state of game"""
-    return self.__str
+        if self == "UNFINISHED":
+            print("No one reached 15. Get ready for next round.")
+        elif self == "FIRST_WON":
+            print("First player won!!!")
+        elif self == "SECOND_WON":
+            print("Second player won!!!")
+        elif self == "DRAW":
+            print("Game ends in draw")
 
-def make_move(self, pl, x):
-    """def that controls when player makes a move."""
-    if x in self.__played:
-        return False
-    if x > 9 or x < 1:
-        return False
-    if pl == "first":
-        self.__pl = self.__p1 + x
-        self.__played.append(x)
-    elif pl == "second":
-        self.__p2 = self.__p2 + x
-        self.__played.append(x)
-    if self.__p1 == 15 and self.__p2 == 15:
-        self.__str = "DRAW"
-    elif self.__p1 == 15:
-        self.__str = "FIRST_WON"
-    elif self.__p2 == 15:
-        self.__str = "SECOND_WON"
-    if len(self.__played) == 9:
-        self.__str = "DRAW"
-    return True
+        return self.__str
 
-game = str(AddThreeGame())
-while True:
-    x = int(input("Player 1 please enter a number: "))
-while True:
-    if game.make_move("first", x) == True:
-        else x = int(input("Invalid input! Player 2 please re-enter a number: "))
-
-state = game.get_current_state()
-if state == "UNFINISHED":
-    print("No one reached 15. Get ready for next round.")
-elif state == "FIRST_WON":
-    print("First player won!!!")
-elif state == "SECOND_WON":
-    print("Second player won!!!")
-elif state == "DRAW":
-    print("Game ends in draw")
-
+    def make_move(self, pl, x):
+        """def that controls when player makes a move."""
+        if x in self.__played:
+            return False
+        if x > 9 or x < 1:
+            return False
+        if pl == "first":
+            self.__p1 = self.__p1 + x
+            self.__played.append(x)
+        elif pl == "second":
+            self.__p2 = self.__p2 + x
+            self.__played.append(x)
+        if self.__p1 == 15 and self.__p2 == 15:
+            self.__str = "DRAW"
+        elif self.__p1 == 15:
+            self.__str = "FIRST_WON"
+        elif self.__p2 == 15:
+            self.__str = "SECOND_WON"
+        if len(self.__played) == 9:
+            self.__str = "DRAW"
+        while True:
+            x = int(input("Player 1 please enter a number: "))
+        while True:
+            if game.make_move("first", x):
+                else x = int("Invalid input! Player 2 please re-enter a number: ")
